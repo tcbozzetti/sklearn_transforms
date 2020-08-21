5add3e58641c529b6673ba6947b470f87a32af19
Tiago Bozzetti <tcbozzetti@gmail.com>
2020-08-21 20:16:53 -0300

    Add custom transformations

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -14,3 +14,29 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+
+class FillMissingGrade(BaseEstimator, TransformerMixin):
+    def fill(self, row):
+        if math.isnan(row['NOTA_GO']):
+            return (row['NOTA_DE'] + row['NOTA_EM'] + row['NOTA_MF']) / 3
+        else:
+            return row['NOTA_GO']
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        data = X.copy()
+        data['NOTA_GO'] = data.apply(self.fill, axis=1)
+        return data
+
+class NormalizeColumn(BaseEstimator, TransformerMixin):
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        data = X.copy()
+        data['INGLES'].fillna('NAO_INFORMADO', inplace = True)
+        data['INGLES'] = data['INGLES'].replace([0], 'NAO')
+        data['INGLES'] = data['INGLES'].replace([1], 'SIM')
+        return pd.get_dummies(data, columns=['INGLES'])
